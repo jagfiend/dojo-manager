f9d3ebdaf4a92b58ae5e00263b3d23d0666f66f9
Pete Wond <pete@alberon.co.uk>
2022-11-24 17:38:39 +0000

    Build out member delete controller and tests

--- app/Http/Controllers/Members/DeleteController.php
+++ app/Http/Controllers/Members/DeleteController.php
@@ -5,12 +5,23 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Members;
 
 use App\Http\Controllers\Controller;
+use App\Models\Member;
+use Exception;
 use Illuminate\Http\RedirectResponse;
+use Log;
 
 class DeleteController extends Controller
 {
-    public function __invoke(): RedirectResponse
+    public function __invoke(Member $member): RedirectResponse
     {
-        return to_route(route('members.index'));
+        try {
+            $member->delete();
+            session()->flash('success', 'Member deleted');
+        } catch (Exception $e) {
+            Log::error($e); // @phpstan-ignore-line
+            session()->flash('warning', 'Something went wrong');
+        }
+
+        return to_route('members.index');
     }
 }
---
+++ tests/Feature/Members/DeleteControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Members;
+
+use App\Models\Member;
+use App\Models\User;
+use Tests\TestCase;
+
+class DeleteControllerTest extends TestCase
+{
+    /** @test */
+    public function guest_cannot_delete_member(): void
+    {
+        $member = Member::factory()->create();
+
+        $this
+            ->delete(route('members.delete', $member))
+            ->assertRedirect(route('login'));
+    }
+
+    /** @test */
+    public function authenticated_can_delete_member(): void
+    {
+        $user = User::factory()->create();
+
+        $member = Member::factory()->create();
+
+        $this
+            ->actingAs($user)
+            ->delete(route('members.delete', $member))
+            ->assertRedirectToRoute('members.index')
+            ->assertSessionHas('success', 'Member deleted');
+
+        $this->assertDatabaseCount('members', 0);
+    }
+}
