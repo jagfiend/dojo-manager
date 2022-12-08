8df8f43d28c965ee9fe3d82be9964a724437b3fe
Pete Wond <pete@alberon.co.uk>
2022-12-08 20:40:47 +0000

    Working on create page

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -4,11 +4,19 @@ defineProps({
         type: String,
         default: 'submit',
     },
+    isDisabled: {
+        type: Boolean,
+        default: false,
+    },
 });
 </script>
 
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
+    <button
+        class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150 disabled:cursor-not-allowed"
+        :disabled="isDisabled"
+        :type="type"
+    >
         <slot />
     </button>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\Members\CreateController as MemberCreateController;
-use App\Http\Controllers\Members\DeleteController as MemberDeleteController;
-use App\Http\Controllers\Members\EditController as MemberEditController;
-use App\Http\Controllers\Members\IndexController as MemberIndexController;
-use App\Http\Controllers\Members\StoreController as MemberStoreController;
-use App\Http\Controllers\Members\UpdateController as MemberUpdateController;
+use App\Http\Controllers\Members;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Route;
 
@@ -23,17 +18,17 @@ use Illuminate\Support\Facades\Route;
 
 require __DIR__ . '/auth.php';
 
-Route::get('/', fn (): RedirectResponse => to_route('dashboard'));
+Route::get('/', fn(): RedirectResponse => to_route('dashboard'));
 
 Route::middleware(['auth', 'verified'])->group(function (): void {
     Route::get('/dashboard', DashboardController::class)->name('dashboard');
 
     Route::prefix('members')->group(function (): void {
-        Route::get('/', MemberIndexController::class)->name('members.index');
-        Route::get('/create', MemberCreateController::class)->name('members.create');
-        Route::post('/', MemberStoreController::class)->name('members.store');
-        Route::get('/{member}/edit', MemberEditController::class)->name('members.edit');
-        Route::put('/{member}', MemberUpdateController::class)->name('members.update');
-        Route::delete('/{member}', MemberDeleteController::class)->name('members.delete');
+        Route::get('/', Members\IndexController::class)->name('members.index');
+        Route::get('/create', Members\CreateController::class)->name('members.create');
+        Route::post('/', Members\StoreController::class)->name('members.store');
+        Route::get('/{member}/edit', Members\EditController::class)->name('members.edit');
+        Route::put('/{member}', Members\UpdateController::class)->name('members.update');
+        Route::delete('/{member}', Members\DeleteController::class)->name('members.delete');
     });
 });
