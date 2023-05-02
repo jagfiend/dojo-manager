31801965967857446e9085b0f8bf08d1dc907b9c
Pete Wond <pete@alberon.co.uk>
2023-05-02 15:18:48 +0100

    More sniffs and some skips for Laravel boilerplate

--- ecs.php
+++ ecs.php
@@ -8,6 +8,7 @@ use PHP_CodeSniffer\Standards\Generic\Sniffs\NamingConventions\CamelCapsFunction
 use PHP_CodeSniffer\Standards\Generic\Sniffs\PHP\ForbiddenFunctionsSniff;
 use PHP_CodeSniffer\Standards\PSR12\Sniffs\ControlStructures\BooleanOperatorPlacementSniff;
 use PHP_CodeSniffer\Standards\Squiz\Sniffs\ControlStructures\ControlSignatureSniff;
+use PHP_CodeSniffer\Standards\Squiz\Sniffs\PHP\CommentedOutCodeSniff;
 use SlevomatCodingStandard\Sniffs\Arrays\TrailingArrayCommaSniff;
 use SlevomatCodingStandard\Sniffs\Classes\ClassConstantVisibilitySniff;
 use SlevomatCodingStandard\Sniffs\Commenting\EmptyCommentSniff;
@@ -19,6 +20,18 @@ use SlevomatCodingStandard\Sniffs\Exceptions\DeadCatchSniff;
 use SlevomatCodingStandard\Sniffs\Functions\UnusedInheritedVariablePassedToClosureSniff;
 use SlevomatCodingStandard\Sniffs\Functions\UnusedParameterSniff;
 use SlevomatCodingStandard\Sniffs\Functions\UselessParameterDefaultValueSniff;
+use SlevomatCodingStandard\Sniffs\Namespaces\AlphabeticallySortedUsesSniff;
+use SlevomatCodingStandard\Sniffs\Namespaces\MultipleUsesPerLineSniff;
+use SlevomatCodingStandard\Sniffs\Namespaces\UnusedUsesSniff;
+use SlevomatCodingStandard\Sniffs\Namespaces\UseFromSameNamespaceSniff;
+use SlevomatCodingStandard\Sniffs\PHP\ShortListSniff;
+use SlevomatCodingStandard\Sniffs\PHP\TypeCastSniff;
+use SlevomatCodingStandard\Sniffs\PHP\UselessParenthesesSniff;
+use SlevomatCodingStandard\Sniffs\PHP\UselessSemicolonSniff;
+use SlevomatCodingStandard\Sniffs\TypeHints\LongTypeHintsSniff;
+use SlevomatCodingStandard\Sniffs\TypeHints\NullableTypeForNullDefaultValueSniff;
+use SlevomatCodingStandard\Sniffs\TypeHints\ParameterTypeHintSpacingSniff;
+use SlevomatCodingStandard\Sniffs\TypeHints\ReturnTypeHintSpacingSniff;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
@@ -39,23 +52,38 @@ return static function (ECSConfig $ecsConfig): void
     ]);
 
     $ecsConfig->rules([
+        AlphabeticallySortedUsesSniff::class,
         CamelCapsFunctionNameSniff::class, // functionsNamedLikeThis()
         ClassConstantVisibilitySniff::class, // must be fixed manually
         DeadCatchSniff::class,
         EarlyExitSniff::class,
         EmptyCommentSniff::class,
+        LongTypeHintsSniff::class,
+        MultipleUsesPerLineSniff::class, // this is in conflict with Laravel approach
         NewWithParenthesesSniff::class,
+        NullableTypeForNullDefaultValueSniff::class,
+        ParameterTypeHintSpacingSniff::class,
+        ShortListSniff::class,
         RequireNullCoalesceOperatorSniff::class,
+        ReturnTypeHintSpacingSniff::class,
         TrailingArrayCommaSniff::class,
+        TypeCastSniff::class,
         UnusedInheritedVariablePassedToClosureSniff::class,
         UnusedParameterSniff::class,
+        UnusedUsesSniff::class,
+        UseFromSameNamespaceSniff::class,
         UselessFunctionDocCommentSniff::class,
         UselessParameterDefaultValueSniff::class,
+        UselessParenthesesSniff::class,
+        UselessSemicolonSniff::class,
     ]);
 
     $ecsConfig->rulesWithConfiguration([
         BooleanOperatorPlacementSniff::class => [
             'allowOnly' => 'last', // put && or || at the end of multi line conditionals
+        ],
+        CommentedOutCodeSniff::class => [
+            'maxPercentage' => 25,
         ],
         ControlSignatureSniff::class => [
             'requiredSpacesBeforeColon' => 0,
@@ -79,12 +107,17 @@ return static function (ECSConfig $ecsConfig): void
 
     $ecsConfig->skip([
         CamelCapsFunctionNameSniff::class => [
-            '/tests/*' // test_methods_named_like_this()
+            __DIR__ .  '/tests/*', // test_methods_named_like_this()
+        ],
+        CommentedOutCodeSniff::class => [
+            __DIR__ . '/app/Http/Kernel.php',
+            __DIR__ . '/app/Http/Middleware/ValidateSignature.php',
+            __DIR__ . '/config/*',
         ],
         UnusedParameterSniff::class => [
-            'app/Console/Kernel.php',
-            'app/Exceptions/Handler.php',
-            'database/factories/*' // ignore unused $attributes param
+            __DIR__ . '/app/Console/Kernel.php',
+            __DIR__ . '/app/Exceptions/Handler.php',
+            __DIR__ . '/database/factories/*', // ignore unused $attributes param
         ]
     ]);
 };
